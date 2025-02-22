1a0b561cdae8393fb9a99d81500121106bdf3c93
david????#6026 <113543570+david6026@users.noreply.github.com>
2025-02-22 10:27:40 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -6,8 +6,8 @@ local MeteorFolder, ConfigsFolder = "Meteor", "Meteor/Configs"
 if not isfolder(MeteorFolder) then do makeFolder(MeteorFolder)
 if not isfolder(ConfigsFolder) then do makeFolder(ConfigsFolder)
     
-    if isfolder(MeteorFolder) and isfolder(ConfigsFolder) then do
-        if game.PlaceId = 6872274481 then do
+    if isfolder(MeteorFolder) and isfolder(ConfigsFolder) then
+        if game.PlaceId = 6872274481 then
             loadstring(game:HttpGet("https://raw.githubusercontent.com/ScrapedMeteor/MeteorForRoblox/refs/heads/main/Games/bedwars.lua"))()
         end
     end
