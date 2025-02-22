4967bf005d429981e4e126b9effe62a22af609d2
ScrapedMeteor <osamaobada22@gmail.com>
2025-02-22 11:12:30 +0200

    Added loader script

---
+++ loader.lua
@@ -0,0 +1,14 @@
+local Players = game:GetService("Players")
+local lplr = Players.LocalPlayer
+local HttpService = game:GetService("HttpService")
+local MeteorFolder, ConfigsFolder = "Meteor", "Meteor/Configs"
+
+if not isfolder(MeteorFolder) then do makeFolder(MeteorFolder)
+if not isfolder(ConfigsFolder) then do makeFolder(ConfigsFolder)
+    
+    if isfolder(MeteorFolder) and isfolder(ConfigsFolder) then do
+        if game.PlaceId = 6872274481 then do
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/ScrapedMeteor/MeteorForRoblox/refs/heads/main/Games/bedwars.lua"))()
+        end
+    end
+end
