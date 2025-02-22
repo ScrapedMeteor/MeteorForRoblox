72f72923c1f8a0d03da896d21ceb43a7eb8e033d
david????#6026 <113543570+david6026@users.noreply.github.com>
2025-02-22 10:28:21 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -3,8 +3,8 @@ local lplr = Players.LocalPlayer
 local HttpService = game:GetService("HttpService")
 local MeteorFolder, ConfigsFolder = "Meteor", "Meteor/Configs"
 
-if not isfolder(MeteorFolder) then do makeFolder(MeteorFolder)
-if not isfolder(ConfigsFolder) then do makeFolder(ConfigsFolder)
+if not isfolder(MeteorFolder) then makeFolder(MeteorFolder)
+if not isfolder(ConfigsFolder) then makeFolder(ConfigsFolder)
     
     if isfolder(MeteorFolder) and isfolder(ConfigsFolder) then
         if game.PlaceId = 6872274481 then
